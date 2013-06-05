67c37c5bc4809e8f2e9650deff2e75c085c281fe
Joseph Sheedy <joseph.sheedy@gmail.com>
2013-06-04 17:05:25 -0700

    updates

--- lectures/06-04-2013_debuggers_and_review/examples/ipython-debug/conditional.py
+++ lectures/06-04-2013_debuggers_and_review/examples/ipython-debug/conditional.py
@@ -2,7 +2,6 @@ import random
 
 def worker(x):
     print "entered worker"
-    1/0
     return x
 
 def main(x):
@@ -11,4 +10,5 @@ def main(x):
     return worker(x+y)
 
 if __name__ == "__main__":
-    print main(random.random())
+    while True:
+        main(random.random())
